381a64617a7b28e97ee7624f7e04c3068d4d5f2b
bhram999 <bh.ram999@gmail.com>
2022-09-04 10:13:10 -0600

    create scaffolds for books and stores

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class StoreController < ApplicationController
+class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home_page]
   def home_page
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :books
+  resources :stores
   devise_for :users
-  get 'store/home_page'
-  get 'store/dashboard'
+  get 'home/home_page'
+  get 'home/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "store#home_page"
+  root "home#home_page"
 end
