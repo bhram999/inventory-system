b54c03a2b739be888b6b213c78332268194f5407
bhram999 <bh.ram999@gmail.com>
2022-09-04 09:43:35 -0600

    rename controllers and routes

--- app/controllers/static_pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:landing_page]
-  def landing_page
-  end
-
-  def dashboard
-  end
-end
---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,8 @@
+class StoreController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home_page]
+  def home_page
+  end
+
+  def dashboard
+  end
+end
