df94e2c230d0fe0c844818215efad449318cca39
bhram999 <bh.ram999@gmail.com>
2022-09-04 09:25:27 -0600

    add models and login only for dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_04_144213) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_04_151104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.integer "stock"
+    t.bigint "store_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["store_id"], name: "index_books_on_store_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stores_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_04_144213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "stores"
+  add_foreign_key "stores", "users"
 end
